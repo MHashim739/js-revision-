1c7492869342300d8930e4a5de13314f7823effc
Muhammad hashim <mh4165209@gmail.com>
2026-06-24 16:25:03 +0500

    craeet 2 file html and css and 5 to 10 line write code i n js

---
+++ app.js
@@ -0,0 +1,16 @@
+// alert("ajj javascript revision ka first day ha");
+// var namefull="Muhammed hashim";
+// alert(namefull)
+// var namefull;
+// namefull="M Kaif";
+// alert(namefull);
+// var nameeFull="Muhammed Ahsan";
+// var namePersonOne="Kaif Raza";
+// alert (nameeFull);
+// alert (namePersonOne);
+// var phNumber="03442077931";
+// var ordiNumber=10;
+// alert(phNumber);
+// alert(ordiNumber)
+// var aisaNumber=3442077931;
+// alert(aisaNumber)
