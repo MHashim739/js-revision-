e1d9ceef47961b4f17cac4e58b20f46646e936c8
Muhammad hashim <mh4165209@gmail.com>
2026-06-28 12:33:46 +0500

    day 4 hald day complete in js revision

--- app.js
+++ app.js
@@ -33,21 +33,41 @@
 // const FullMilingAddress=` ${street} ${country}`;
 // console.log(fullMilingAddress);
 // console.log(FullMilingAddress);
-var score1=20;
-let score2=30;
-const score3=40;
-const totalScore= score1 + score2 + score3;
-const averageScore=totalScore /3;
-console.log(totalScore);
-console.log(averageScore);
+// var score1=20;
+// let score2=30;
+// const score3=40;
+// const totalScore= score1 + score2 + score3;
+// const averageScore=totalScore /3;
+// console.log(totalScore);
+// console.log(averageScore);
 
-let plates=20;
-const people=7;
-let remainingPlates=plates % people;
-remainingPlates +=1;
-const message=`There are your Value goes here ${remainingPlates}`
-console.log(message);
+// let plates=20;
+// const people=7;
+// let remainingPlates=plates % people;
+// remainingPlates +=1;
+// const message=`There are your Value goes here ${remainingPlates}`
+// console.log(message);
+// const userInput=prompt(`Enter Your Name`);
+// const result=userInput;
+// if(userInput === result){
+//     let num1=1;
+// let num2=2;
+// let num3=3;
+// const totalNumber=(num1 + num2) * num3;
+// const totalNumber1=(num1 + num2) - num3;
+// const totalNumber2=(num1 * num2) + num3;
+// const totalNumber3=(num1 - num2) + num3;
+// console.log(`BODMS apply in total number sum after multiply ANSWERS ${totalNumber}`);
+// console.log(`BODMS apply in total number 01 sum after subtract ANSWERS ${totalNumber1}`);
+// console.log(`BODMS apply in total number 02  multiply after sum ANSWERS ${totalNumber2}`);
+// console.log(`BODMS apply in total number02 subtract after sum ANSWERS  ${totalNumber3}`);
+// }else{
+//     alert(`INVALID ERROR`);
+// }
 
+//data type in javascript
+
+//string// boolean // numbers// array //objects //null // undefined
 
 
 
