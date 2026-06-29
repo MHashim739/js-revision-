1fb3f5f5dc1a21e742bcfb684a60d6a5009b29e7
Muhammad hashim <mh4165209@gmail.com>
2026-06-29 23:57:37 +0500

    day 6 half complete js revision

--- app.js
+++ app.js
@@ -68,15 +68,122 @@
 //data type in javascript
 
 //string// boolean // numbers// array //objects //null // undefined
-var lunckahogey=prompt(`ha dost lunch ka time ha ammi pouch rahi ha ka lunch kahu ga kia`)
-if(lunckahogey === "haan"){
-    var selectionDost=prompt(`ha dost kia kahu ha lunch  ma biryani , korma , ploa`)
-    if(selectionDost==selectionDost){
-        alert(`ammi app kahana ma  ${selectionDost} laga da`)
-    }
-}else{
-    alert(`ammi dost na mana kar diyaha lunch khana sa`)
-}
+// var lunckahogey=prompt(`ha dost lunch ka time ha ammi pouch rahi ha ka lunch kahu ga kia`)
+// if(lunckahogey === "haan"){
+//     var selectionDost=prompt(`ha dost kia kahu ha lunch  ma biryani , korma , ploa`)
+//     if(selectionDost==selectionDost){
+//         alert(`ammi app kahana ma  ${selectionDost} laga da`)
+//     }
+// }else if(){
+//     alert(`ammi dost na mana kar diyaha lunch khana sa`)
+// }
+// var userSignIn=prompt("you are sign in this platform");
+// if(userSignIn =="yes"){
+//     var UserPassword=prompt("please set your password");
+//     console.log(UserPassword);
+// }else{
+//     alert(`${userSignIn} ok me found nto signin`);
+//     console.log(`${userSignIn} ok me found nto signin`);
+    
+// }
+
+// var question=prompt(" ham app sa school sa mutalik question kar sakta ha kia")
+// if(question == "yes"){
+//     if(true){
+//     var question01=prompt("app jata hu school");
+//     if(question01 ==question01){
+//         var question02=prompt("you school going app kon si class ma hu");
+//         console.log(question02);
+        
+//     if(question02 == question02){
+//       var question03=prompt("app ka school ka name kia ha","gulistan");
+//       if(question03==question03){
+//         var question04=prompt("app ki school ma repotation kaisy ha");
+//         if(question04=="yes"){
+        
+//         }
+//       }
+//     }    
+//     }
+// }
+// }
+// var num1=2;
+// num1++;
+// console.log(num1);
+// ++num1;
+// console.log(num1);
+// --num1;
+// console.log(num1);
+// --num1;
+// console.log(num1);
+// num1 +=1;
+// console.log(num1);
+// num1 *=2;
+// console.log(num1);
+// num1 +=10;
+// console.log(num1);
+// num1 -=4;
+// console.log(num1);
+// num1 %=3;
+// console.log(num1);
+// num1 *=2;
+// console.log(num1);
+// let num1=20;
+// num1++;
+// ++num1;
+// num1++;
+// num1++;
+// console.log(num1);
+// num1 -=8;
+// console.log(num1);
+// num1 %= 5;
+// console.log(num1);
+// num1 *=2;
+// console.log(num1);
+// let name=prompt("enter your name");
+// let name2=prompt("enter your father name");
+// alert(`i am a happy for you  ${name} becau you fill this 2 input field`)
+// console.log(`i am a happy for you  ${name} becauyou fill this 2 input fill`);
+// if(name = name2){
+//     let identifyNameCheck=prompt("once more please enter your name");
+//     let identifyFaName=prompt("once more please enter your father name")
+//     alert(`allow ${name} in his son ${name2}`);
+//     console.log(`allow ${name} in his son ${name2}`);
+    
+
+// }else{
+//     alert(`not axcess ${name} in his son ${name2}`)
+//     console.log(`not axcess ${name} in his son ${name2}`);
+    
+// }
+// var age=prompt(`dost what is your age`);
+// console.log(typeof(age));
+
+// var isBlind=prompt(`dost kia app blind hu`);
+// if(age >=18 && isBlind == "no"){
+//     alert(`lisence  allowed ${age} ${isBlind}`)
+//     console.log(`lisence  allowed ${age} ${isBlind}`);
+    
+// }else{
+//     alert(`lisence not allowed ${age} ${isBlind}`);
+//     console.log(`lisence not allowed because your age is this ${age}`);
+    
+// }
+// var allCssAssigmentDone=prompt("are you css all assigment done");
+
+// var yourAttendence=prompt("your attendence is complete");
+
+// var yourHakathonIsPassed=prompt("your css hakathon pass ");
+
+// if(allCssAssigmentDone =="yes" && yourAttendence=="yes" || yourHakathonIsPassed=="yes"){
+//     alert("you allowed in javascript module");
+//     console.log("you allowed in javascript module");
+    
+// }else{
+//     alert("you need in repeat topics")
+//     console.log("you need in repeat topics");
+    
+// }
 
 
 
